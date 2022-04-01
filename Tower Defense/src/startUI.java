35e27ff71f5037436ee561c053870f8e8060b7b8
Eddi1405 <edwardsiebert53@gmail.com>
2022-04-01 11:52:04 +0200

    new button

--- Tower Defense/src/startUI.java
+++ Tower Defense/src/startUI.java
@@ -20,42 +20,49 @@ public class startUI extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
 
+
+
+        //Startbutton design
         startButton.setFocusPainted(false);
         startButton.setBorderPainted(false);
         startButton.setContentAreaFilled(false);
+        //ändert das icon wenn gedrückt
+        try {
+            Image img = ImageIO.read(getClass().getResource("Bilder/button_start.1.png"));
+            startButton.setPressedIcon(new ImageIcon(img));
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
 
-        exitButton.setFocusPainted(false);
-        exitButton.setBorderPainted(false);
-        exitButton.setContentAreaFilled(false);
-
-        //Listener Event wartet auf User Aktion - Startknopf
+        //Es wird geprüft ob der Button start gedrückt wurde
         startButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Start");
 
-                try {
-                    Image img = ImageIO.read(getClass().getResource("Bilder/button_start_2.png"));
-                    startButton.setPressedIcon(new ImageIcon(img));
-                } catch (Exception ex) {
-                    System.out.println(ex);
-                }
+
             }
         });
 
+        //Exitbutton design
+        exitButton.setFocusPainted(false);
+        exitButton.setBorderPainted(false);
+        exitButton.setContentAreaFilled(false);
+        //ändert das icon wenn gedrückt
+        try {
+            Image img = ImageIO.read(getClass().getResource("Bilder/button_exit.1.png"));
+            exitButton.setPressedIcon(new ImageIcon(img));
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
 
+        //Es wird geprüft ob der Button exit gedrückt wurde
         exitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
                 System.out.println("Exit");
 
-                try {
-                    Image img = ImageIO.read(getClass().getResource("Bilder/button_exit_2.png"));
-                    exitButton.setPressedIcon(new ImageIcon(img));
-                } catch (Exception ex) {
-                    System.out.println(ex);
-                }
             }
         });
     }
