1e104c4fdca33a9446022b77d945d2457d603d77
Eddi1405 <77676438+Eddi1405@users.noreply.github.com>
2022-04-07 15:42:41 +0200

    Add files via upload

---
+++ Tower Defense/src/Mapmenu.java
@@ -0,0 +1,38 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import javax.swing.JFrame;
+
+public class Mapmenu {
+
+    private JButton Map1;
+    private JButton Map2;
+    private JButton Map3;
+    public JPanel panel2;
+
+
+    public Mapmenu() {
+        Map1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        Map2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        Map3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
+
---
+++ Tower Defense/src/mapGen.java
@@ -0,0 +1,132 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class mapGen {
+
+    GamePanel gp;
+    Tile[] tile;
+    int mapTileNum[][];
+
+    public mapGen(GamePanel gp) {
+
+        this.gp = gp;
+        tile = new Tile[20];
+        mapTileNum = new int[gp.maxScreenCol][gp.maxScreenRow];
+        getTileimage();
+        loadMap();
+
+    }
+
+    public void getTileimage() {
+        //Tiles werden defieniert
+        try {
+
+            tile[0] = new Tile();
+            tile[0].image = ImageIO.read(getClass().getResourceAsStream("/Tiles/grass.png"));
+
+            tile[1] = new Tile();
+            tile[1].image = ImageIO.read(getClass().getResourceAsStream("/Tiles/grass1.1.png"));
+
+            tile[2] = new Tile();
+            tile[2].image = ImageIO.read(getClass().getResourceAsStream("/Tiles/grass1.2.png"));
+
+            tile[3] = new Tile();
+            tile[3].image = ImageIO.read(getClass().getResourceAsStream("/Tiles/grass1.3.png"));
+
+            tile[4] = new Tile();
+            tile[4].image = ImageIO.read(getClass().getResourceAsStream("/Tiles/grass2.1.png"));
+
+            tile[5] = new Tile();
+            tile[5].image = ImageIO.read(getClass().getResourceAsStream("/Tiles/grass2.2.png"));
+
+            tile[6] = new Tile();
+            tile[6].image = ImageIO.read(getClass().getResourceAsStream("/Tiles/grass2.3.png"));
+
+            tile[7] = new Tile();
+            tile[7].image = ImageIO.read(getClass().getResourceAsStream("/Tiles/grass3.1.png"));
+
+            tile[8] = new Tile();
+            tile[8].image = ImageIO.read(getClass().getResourceAsStream("/Tiles/grass3.2.png"));
+
+            tile[9] = new Tile();
+            tile[9].image = ImageIO.read(getClass().getResourceAsStream("/Tiles/grass3.3.png"));
+
+            tile[10] = new Tile();
+            tile[10].image = ImageIO.read(getClass().getResourceAsStream("/Tiles/grass3.4.png"));
+
+            tile[11] = new Tile();
+            tile[11].image = ImageIO.read(getClass().getResourceAsStream("/Tiles/grass4.png"));
+
+            tile[12] = new Tile();
+            tile[12].image = ImageIO.read(getClass().getResourceAsStream("/Tiles/baum.png"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Map wird geladen
+    public void loadMap(){
+
+        try {
+            //text datei wird eingelesen
+            InputStream is = getClass().getResourceAsStream("map.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+            int col = 0;
+            int row = 0;
+
+            while (col < gp.maxScreenCol && row < gp.maxScreenRow){
+
+                String line = br.readLine();
+
+                while (col < gp.maxScreenCol ){
+
+                    String numbers[] = line.split(" ");
+
+                    int num = Integer.parseInt(numbers[col]);
+
+                    mapTileNum[col][row] = num;
+                    col++;
+
+                }
+                if(col == gp.maxScreenCol){
+                    col = 0;
+                    row++;
+                }
+            }
+            br.close();
+        }catch (Exception e){}
+    }
+
+    public void draw(Graphics2D g2){
+
+        int col = 0;
+        int row = 0;
+
+        int x = 0;
+        int y = 0;
+
+        while(col < gp.maxScreenCol && row < gp.maxScreenRow){
+
+            int tileNum = mapTileNum[col][row];
+
+            g2.drawImage(tile[tileNum].image, x, y, gp.tileSize, gp.tileSize, null);
+            col++;
+            x += gp.tileSize;
+            if(col == gp.maxScreenCol){
+                col = 0;
+                x = 0;
+                row++;
+                y += gp.tileSize;
+            }
+        }
+    }
+
+    }
+
