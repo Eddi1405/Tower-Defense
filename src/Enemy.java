3d698cf11c91bcbe482fc65b13aac17772f79926
Flo7880 <florian-laufer@web.de>
2022-04-13 11:34:00 +0200

    55

---
+++ src/Enemy.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+
+public class Enemy {
+
+    private double x,y;
+    private Textures tex;
+
+    private int Leben,Resistenz;
+    String Typ;
+
+    public Enemy(double x,double y){
+        //Position
+        this.x = x;
+        this.y = y;
+    }
+//Geschwindigkeit
+    void tick(){
+        y -= 5;
+    }
+
+    public void render(Graphics g){
+        g.drawImage(tex.Enemy,(int)x,(int)y,null);
+    }
+}
---
+++ src/Textures.java
@@ -0,0 +1,19 @@
+import java.awt.image.BufferedImage;
+
+public class Textures {
+//Grober Aufbau (Idee)
+
+    //Bilder Hinterlegen für Variabeln
+public BufferedImage Tower, Enemy;
+
+
+    public Textures(UI_gamepanel gp){
+
+        getTextures();
+    }
+
+    private void getTextures(){
+
+    }
+}
+
