af14a0ee61b5240528296ef1ffeea5d8b2f2bdb8
Eddi1405 <edwardsiebert53@gmail.com>
2022-05-20 15:48:35 +0200

    path

--- src/Gegner.java
+++ src/Gegner.java
@@ -13,8 +13,8 @@ public class Gegner extends Entity {
     public boolean valid;
     public int walkFrame = 0;
     public int walkSpeed = 1;
-    public boolean waagerechterWeg, senkrechterWeg,xLinks,yHoch,xRechts,yRunter;
-    public int i,j;
+    public boolean waagerechterWeg, senkrechterWeg,xLinks,yHoch,xRechts,tile;
+    public int i,j,oldTile;
     //Instanzen
     UI_gamepanel gp;
     path ph;
@@ -31,6 +31,7 @@ public class Gegner extends Entity {
         solidArea = new Rectangle(30, 20, gp.w_TileSize - 60, gp.h_TileSize - 40);
         i = 0;
         j = 0;
+        tile = false;
         //Dynamische größe wird in eine variable gepackt
     }
 
@@ -58,59 +59,51 @@ public class Gegner extends Entity {
         ph.check(this, x, y);
 
 
-        //2,7,4 geht danach geht nichts mehr
+        //1,2,3,4,6,7 geht danach geht nichts mehr. funktioniert auch nur halb. An den Ecken Beschleunigt der Baloon die ganze Zeit und einmal bounct der him und her.
+        //problem: der Baloon fährt nicht richtig um die ecken es wird immer i auf null gesetzt. dadurch fährt der baloon zu weit
         switch (ph.tileNum1) {
             case 1:
-                movePath(0, gp.h_TileSize);
+                moveY(1);
                 senkrechterWeg = true;
-                waagerechterWeg = false;
-                yHoch = true;
                 break;
             case 2:
-                movePath(gp.w_TileSize, 0);
+                moveX(1,2);
                 waagerechterWeg = true;
-                senkrechterWeg = false;
-                xRechts = true;
                 break;
             case 3:
-                //movePath(gp.w_TileSize, 0);
-                waagerechterWeg = false;
-                senkrechterWeg = false;
+                yHoch = true;
+                xLinks = true;
+                moveY(2);
+                moveX(2,3);
                 break;
             case 4:
-                movePath(gp.w_TileSize, gp.h_TileSize/2);
-                waagerechterWeg = false;
-                senkrechterWeg = false;
-                xRechts = true;
+                xLinks = false;
                 yHoch = true;
+                moveX(2,4);
+                moveY(2);
                 break;
             case 5:
-                movePath(0, gp.h_TileSize);
-                waagerechterWeg = false;
-                senkrechterWeg = false;
-
+                moveX(2,5);
+                moveY(2);
                 break;
             case 6:
-                //movePath(gp.w_TileSize, 0);
-                waagerechterWeg = false;
-                senkrechterWeg = false;
+                yHoch = false;
+                xLinks = true;
+                moveX(2,6);
+                moveY(2);
                 break;
             case 7:
-                if (waagerechterWeg) {
-                    movePath(gp.w_TileSize, 0);
-                    xRechts = true;
-
-                } else if (senkrechterWeg) {
-                    movePath(0, gp.h_TileSize);
-                    yRunter = true;
-
+                if(senkrechterWeg){
+                    moveY(1);
+                } else if (waagerechterWeg) {
+                    moveX(1,7);
                 }
                 break;
         }
 
     }
 
-
+    //OLD, für jedes tile wurde eine eigene Klasser erstellt. Damit der switch sauberer aus sieht und das programm übersichtlicher ist.
     public void movePath(int tileW, int tileH) {
         //System.out.println("Ich Fahre vorwärts");
         if (i <= tileW && xRechts) {
@@ -146,7 +139,42 @@ public class Gegner extends Entity {
             System.out.println("yreset");
         }
     }
-
+    public void moveX(int teil,int newTile){
+        if (i <= gp.w_TileSize/teil) {
+            if (walkFrame >= walkSpeed) {
+                if(xLinks){
+                    x = x - 1;
+                }
+                else{
+                    x = x + 1;
+                }
+                i++;
+                walkFrame = 0;
+            } else {
+                walkFrame++;
+            }
+        }else{
+                i = 0;
+                System.out.println("new");
+        }
+    }
+    public void moveY(int teil){
+        if (i <= gp.h_TileSize/teil) {
+            if (walkFrame >= walkSpeed) {
+                if(yHoch){
+                    y = y - 1;
+                }else{
+                    y = y + 1;
+                }
+                i++;
+                walkFrame = 0;
+            } else {
+                walkFrame++;
+            }
+        }else{
+            i = 0;
+        }
+    }
 
     //Zeichnet den Gegner auf dem GamePanel
     public void draw(Graphics2D g2) {
